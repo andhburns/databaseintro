b7a994dca107081c7d299baeb612c41370601805
Doug Odegaard <doug@intralogix.biz>
2016-09-23 22:07:33 -0600

    exercise

--- temp/pg_check.sql
+++ temp/pg_check.sql
@@ -26,3 +26,26 @@ INNER JOIN products AS p on p."ProductID" = od."ProductID"
 INNER JOIN categories AS cat on cat."CategoryID" = p."CategoryID"
 ORDER BY ExtendedPrice DESC, c."CompanyName"
 
+
+
+
+CREATE VIEW public.Orders1997 AS
+SELECT cust."CompanyName",
+    EXTRACT(MONTH FROM o."OrderDate") AS OrderMonth
+    ,cat."CategoryName"
+    ,COUNT(o."OrderID") as OrderCount
+    ,SUM(od."UnitPrice" * od."Quantity") as ExtendedAmount
+    ,MIN(od."UnitPrice" * od."Quantity") as ExtendedMin
+   FROM customers AS cust
+     JOIN orders AS o ON cust."CustomerID" = o."CustomerID"
+     JOIN order_details AS od ON od."OrderID" = o."OrderID"
+     JOIN products AS p ON p."ProductID" = od."ProductID"
+     JOIN categories AS cat ON cat."CategoryID" = p."CategoryID"
+  WHERE EXTRACT(YEAR FROM o."OrderDate") = 1997
+	--AND cust."CompanyName" = 'Save-a-lot Markets'
+  GROUP BY cust."CompanyName",
+     EXTRACT(MONTH FROM o."OrderDate"),
+    cat."CategoryName"
+  HAVING MIN(od."UnitPrice" * od."Quantity") < 250
+  --ORDER BY ordermonth, cat."CategoryName"
+
