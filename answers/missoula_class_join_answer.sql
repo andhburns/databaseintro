b8fd05de1261d5c66828d135e2b7e63514547238
Doug Odegaard <doug@intralogix.biz>
2016-09-24 15:19:44 -0600

    This is the answer to the lab exercise

---
+++ answers/missoula_class_join_answer.sql
@@ -0,0 +1,18 @@
+﻿SELECT CompanyName
+, OrderDate
+, Orders.OrderId
+, ProductName
+, CategoryName
+, Quantity * OD.UnitPrice as ExtendedPrice
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerId = Orders.CustomerId
+INNER JOIN Order_Details AS OD ON Orders.OrderId = OD.OrderId
+INNER JOIN Products ON Products.ProductId = OD.ProductId
+INNER JOIN Categories AS Cat on Products.CategoryId = Cat.CategoryId
+WHERE OrderDate BETWEEN '1996-01-01' AND '1996-12-31'
+--WHERE (OrderDate >= '1996-01-01' AND OrderDate < '1997-01-01')
+--WHERE EXTRACT(YEAR FROM OrderDate) = 1996
+ORDER BY ExtendedPrice DESC
+
+
+
